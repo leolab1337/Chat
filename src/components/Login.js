2016a1fbf5218b01d261ac3a3194bae957e45927
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-02-02 11:55:07 +0200

    login updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { GoogleOutlined,FacebookOutlined } from "@ant-design/icons";
+import firebase from "firebase/app"
 
+
+import { auth } from "../firebase";
 
 const Login = () =>{
     return(
@@ -8,13 +11,15 @@ const Login = () =>{
             <div id="login-card">
                 <h2>Welcome to Mychat!</h2>
 
-                <div className="login-button google">
+                <div className="login-button google"
+                     onClick={()=>auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}>
                     <GoogleOutlined/> Sign In with Google 
                 </div>
 
                 <br/><br/>
                 
-                <div className="login-button facebook">
+                <div className="login-button facebook"
+                     onClick={()=>auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}>
                     <FacebookOutlined/> Sign In with Facebook
                 </div>
 
